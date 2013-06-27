43529c7e30e1ecaf62c5c13c8a9cf066754647d0
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-06-27 13:25:31 -0600

    Linux support

--- Helper_App/Papilio_Programmer.bat
+++ Helper_App/Papilio_Programmer.bat
@@ -2,7 +2,7 @@
 set CYGWIN=nodosfilewarning
 
 cd /D %0\..\bin
-bash.exe %0\..\Papilio_Programmer.sh %1 %0\..
+bash.exe %0\..\bin\Windows_Papilio_Programmer.sh %1 %0\..
 
 REM uncomment the pause below to troubleshoot.
-pause
+REM pause
--- Helper_App/linbin/Linux_Papilio_Programmer.sh
+++ Helper_App/linbin/Linux_Papilio_Programmer.sh
@@ -4,7 +4,7 @@
 
 bitfile=bitfile
 	
-dialog --timeout 3 --title "Papilio Programmer" \
+./dialog --timeout 3 --title "Papilio Programmer" \
 		--menu "Use the arrow keys to select your option:" 15 55 5 \
 		"1" "Program FPGA - Temporary" \
 		"2" "Program SPI Flash - Permanent" \
@@ -16,14 +16,14 @@ case "$return_value" in
 	"1")
 	
 		echo "Programming the FPGA"
-		papilio-prog -v -f "$1" -v
+		./papilio-prog -v -f "$1" -v
 		return_value=$?
 		;;	
 	"2")
 		echo "Programming to SPI Flash"
 		# Find device id and choose appropriate bscan bit file
 	
-		device_id=`papilio-prog -j | awk '{print $9}'`
+		device_id=`./papilio-prog -j | awk '{print $9}'`
 		return_value=$?
 		
 		case $device_id in
@@ -44,21 +44,21 @@ case "$return_value" in
 				;;
 		esac
 
-		papilio-prog -v -f "$1" -b $bscan_bitfile -sa -r
+		./papilio-prog -v -f "$1" -b $bscan_bitfile -sa -r
 		#Cause the Papilio to restart
-		papilio-prog -c
+		./papilio-prog -c
 		return_value=$?
 		;;
 	"3")
 		echo "Copy to Favorites"
 		#TODO figure how to do in Windows and Linux
-		cp $1 "$2/Favorites/"
+		./cp $1 "$2/Favorites/"
 		cd "$2/Favorites/"
 		explorer .	
 		;;
 	*)
 		echo "Programming the FPGA - Default"
-		papilio-prog -v -f "$1" -v
+		./papilio-prog -v -f "$1" -v
 		return_value=$?
 		;;
 		#exit 1
@@ -66,6 +66,6 @@ esac
 		
 if [ $return_value == 1 ] #If programming failed then show error.
 then
-	dialog --timeout 5 --msgbox "The bit file failed to program to the Papilio, please check that the Papilio is plugged into a USB port." 15 55
+	./dialog --timeout 5 --msgbox "The bit file failed to program to the Papilio, please check that the Papilio is plugged into a USB port." 15 55
 	read -n1 -r -p "Press any key to continue..." key
 fi
