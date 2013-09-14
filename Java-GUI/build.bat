5a15802f58fcb23defae676746cacb1a65403f18
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-09-13 18:35:49 -0600

    New build.bat for windows build

---
+++ Java-GUI/build.bat
@@ -0,0 +1,3 @@
+javac -sourcepath src -d src\net\gadgetfactory\papilio\loader src\net\gadgetfactory\papilio\loader\PapilioLoader.java
+jar cfm0 papilio-loader.jar PapilioLoader.mf -C src\net\gadgetfactory\papilio\loader\ .
+java -jar papilio-loader.jar
