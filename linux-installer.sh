cfcb7db6790b237068649eed13ed3ffd7f95d44d
user <user@ubuntu.(none)>
2013-09-17 21:45:37 -0400

    Fixes for linux installer

--- linux-installer.sh
+++ linux-installer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cd Java-GUI
+sudo ./build.sh
 sudo mkdir -p /opt/GadgetFactory/papilio-loader/
-sudo cp -p Java-GUI/papilio-loader.jar Java-GUI/papilio-loader.sh /opt/GadgetFactory/papilio-loader/
+sudo cp -p papilio-loader.jar papilio-loader.sh /opt/GadgetFactory/papilio-loader/
 sudo ln -s /opt/GadgetFactory/papilio-loader/papilio-loader.sh /usr/local/bin/papilio-loader
 
