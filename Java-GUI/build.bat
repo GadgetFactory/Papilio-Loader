c0d978f4df0715cdbc74bc4fbbdf8f66905d17b7
Sergei Potehin <vexilurz@gmail.com>
2018-06-05 09:52:17 +0300

    Artix-7 XC7A100T support added.

--- Java-GUI/build.bat
+++ Java-GUI/build.bat
@@ -1,3 +1,3 @@
-javac -sourcepath src -d src\net\gadgetfactory\papilio\loader src\net\gadgetfactory\papilio\loader\PapilioLoader.java
-jar cfm0 papilio-loader.jar PapilioLoader.mf -C src\net\gadgetfactory\papilio\loader\ .
-java -jar papilio-loader.jar
+"C:\Program Files\Java\jdk1.8.0_131\bin\javac.exe" -sourcepath src -d src\net\gadgetfactory\papilio\loader src\net\gadgetfactory\papilio\loader\PapilioLoader.java
+"C:\Program Files\Java\jdk1.8.0_131\bin\jar.exe" cfm0 papilio-loader.jar PapilioLoader.mf -C src\net\gadgetfactory\papilio\loader\ .
+"C:\Program Files\Java\jdk1.8.0_131\bin\java.exe" -jar papilio-loader.jar
