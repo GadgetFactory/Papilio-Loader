cbd452f2d2e2bc39705cc0a4b512245bff9123c4
Brad Robinson <brobinson@toptensoftware.com>
2019-05-02 20:14:43 +1000

    Renamed linux32 folder to linux

--- linux-installer.sh
+++ linux-installer.sh
@@ -5,5 +5,5 @@ cd Java-GUI
 sudo ./build.sh
 sudo mkdir -p /opt/GadgetFactory/papilio-loader/
 sudo cp -pr papilio-loader.jar papilio-loader.sh programmer/ images/ help/ /opt/GadgetFactory/papilio-loader/
-sudo ln -s /opt/GadgetFactory/papilio-loader/papilio-loader.sh /usr/local/bin/papilio-loader-gui
-sudo ln -s /opt/GadgetFactory/papilio-loader/programmer/linux32/papilio-prog /usr/local/bin/papilio-prog
+sudo ln -sf /opt/GadgetFactory/papilio-loader/papilio-loader.sh /usr/local/bin/papilio-loader-gui
+sudo ln -sf /opt/GadgetFactory/papilio-loader/programmer/linux/papilio-prog /usr/local/bin/papilio-prog
